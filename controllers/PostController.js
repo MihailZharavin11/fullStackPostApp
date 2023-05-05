ae5127986e9842ceac54de5f4340d89b83532a69
Mikhail <mihail.zharavin65@gmail.com>
2023-05-05 11:53:45 +0300

    Created functionality CRUD and Auth
    Created functionality for CRUD operations of posts and added user registration and authorization

---
+++ controllers/PostController.js
@@ -0,0 +1,109 @@
+import PostModel from "../models/post.js";
+
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate("user").exec();
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({
+      message: "Не удалось посты",
+    });
+  }
+};
+
+export const getPost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    if (!postId) {
+      return res.status(404).json({
+        message: "Статьи с таким id не найдено",
+      });
+    }
+    const post = await PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: "after",
+      }
+    );
+    res.json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не удалось получить статью",
+    });
+  }
+};
+
+export const createPost = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      ...req.body,
+      user: req.userId,
+    });
+
+    const post = await doc.save();
+    res.json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не удалось создать пост",
+    });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const result = await PostModel.findOneAndDelete({
+      _id: postId,
+    });
+
+    if (result) {
+      return res.json({
+        message: "Статья удалена",
+      });
+    } else {
+      return res.status(404).json({
+        message: "Статьи с таким id не найдено",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не удалось удалить статью",
+    });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const result = await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        ...req.body,
+      }
+    );
+    if (result) {
+      return res.json({
+        message: "Статья обновлена",
+      });
+    } else {
+      return res.status(404).json({
+        message: "Статьи с таким id не найдено",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не удалось обновить статью",
+    });
+  }
+};
---
+++ index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import mongoose from "mongoose";
+import multer from "multer";
+import {
+  loginValidation,
+  registerValidation,
+  postValidation,
+} from "./validations/validations.js";
+import checkAuth from "./utils/checkAuth.js";
+import { register, getMe, login } from "./controllers/UserController.js";
+import {
+  createPost,
+  deletePost,
+  getAllPosts,
+  getPost,
+  updatePost,
+} from "./controllers/PostController.js";
+
+mongoose
+  .connect(
+    `mongodb+srv://admin:123qwerty@cluster0.bdyu00y.mongodb.net/blog?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("DB started!");
+  })
+  .catch((err) => {
+    console.log("DB Error", err);
+  });
+
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+});
+
+app.use(express.json());
+
+app.post("/auth/login", loginValidation, login);
+app.post("/auth/register", registerValidation, register);
+app.get("/auth/me", checkAuth, getMe);
+
+app.get("/posts", getAllPosts);
+app.get("/post/:id", getPost);
+app.post("/posts", checkAuth, postValidation, createPost);
+app.delete("/post/:id", checkAuth, deletePost);
+app.patch("/post/:id", checkAuth, updatePost);
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Server started!");
+});
